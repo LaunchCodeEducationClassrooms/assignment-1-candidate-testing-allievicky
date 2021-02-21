5bd2523acef8308db70d0a33b3437e6b1c0cc471
Allie Cannon <allie.vicky@gmail.com>
2021-02-21 01:40:36 +0000

    change candidateName variable to empty with quotes

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,7 +4,7 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 
-let candidateName = [];
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
 let question = "Who was the first American woman in space? ";
@@ -13,7 +13,7 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = " ";
+let candidateAnswers = [];
 
 
 function askForName() {
